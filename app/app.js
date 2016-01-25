bab59c66d627a122c9d91c88d4158eee0cf20c63
Sean Hoilett <seanhoilett@gmail.com>
2016-01-25 09:28:20 -0400

    Setting up the controller

--- app/app.js
+++ app/app.js
@@ -7,3 +7,23 @@ angular.module(appName).run(function () {
 });
 
 
+angular.module(appName).controller(['$scope', function($scope){
+    $scope.email = {
+        recipient: '',
+        message: '',
+        sender: ''
+    };
+
+    $scope.sendEmail = function() {
+
+    };
+
+    $scope.saveDraft = function() {
+
+    };
+
+    $scope.discard = function() {
+        
+    }
+
+}]);
